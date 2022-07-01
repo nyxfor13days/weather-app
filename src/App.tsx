d7e4a5ced5d18768e894ae73fc5cb0ad6d48b3b2
nyxfor13days <harsh1739371@gmail.com>
2022-07-01 23:31:17 +0530

    added react-router-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
-const App: React.FC = () => {
-	return <div>App</div>;
+const App = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+		</Routes>
+	);
 };
 
 export default App;
+
